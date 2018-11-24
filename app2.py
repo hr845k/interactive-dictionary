73994137642e019874a12a2bc1722978a41ccf7c
Hasan Rauf <hr845k@att.com>
2018-11-24 13:59:55 -0500

    "updated the function"

---
+++ app2.py
@@ -0,0 +1,43 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open('data.json'))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean %s instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])
+    if yn == "Y":
+        return data[get_close_matches(w, data.keys())[0]]
+    elif yn == "N":
+        return "The word doesn't exist. Please double check it."
+    else:
+        return "We didn't understand your entry."
+
+search = 3
+word = ''
+
+while (search != 1):
+    print("Enter 1 to quit the dictionary\n")
+    print("Enter 2 to Search the word in the Dictionary\n")
+    search = int(input("Option:"))
+
+    if search == 2:
+        word = input("Enter the Word to search in Dictionary:")
+        output = translate(word)
+        if type(output) == list:
+            for item in output:
+                print(item)
+            else:
+                print(output)
+    elif search == 1:
+        break
+    else:
+        print("You did not enter 1 or 2 you selected wrong option please try again\n")
+
+
+
+
+
